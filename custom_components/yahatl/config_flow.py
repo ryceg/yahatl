dbedb44af193a89a49ad842b6202bbe4f9289f59
Rhys <36913739+ryceg@users.noreply.github.com>
2026-01-01 13:38:17 +1100

    Fix OptionsFlow config_entry property error for newer HA versions

--- custom_components/yahatl/config_flow.py
+++ custom_components/yahatl/config_flow.py
@@ -58,15 +58,11 @@ class YahatlConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         config_entry: config_entries.ConfigEntry,
     ) -> config_entries.OptionsFlow:
         """Create the options flow."""
-        return YahatlOptionsFlow(config_entry)
+        return YahatlOptionsFlow()
 
 
 class YahatlOptionsFlow(config_entries.OptionsFlow):
     """Handle YAHATL options."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
